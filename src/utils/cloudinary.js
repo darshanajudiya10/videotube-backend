a6d907210e606c243b571bc027f3f56b52c9dffd
Darshan_Ajudiya <darshanajudiya257@gmail.com>
2026-04-04 09:39:48 +0530

    add cloudinary utillity and write fist middleware on multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import cloudinary from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const resourse = await cloudinary.v2.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        }) 
+        //file has been uploaded successfully
+        console.log("file is uploaded on cloudinary",response.url);
+        return reponse;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally save as the upload operation got failed
+        return null;
+    }
+}
+
+export default uploadToCloudinary;
